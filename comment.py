6fe932a882cee4307127b0f2ad683dd2076403a5
abhilash <chinni.muthineni@gmail.com>
2017-06-23 13:08:42 -0400

    changes in comment not reflected

--- comment.py
+++ comment.py
@@ -5,3 +5,4 @@ class Comment(db.Model):
     user_id=db.IntegerProperty(required=True)
     post_id=db.StringProperty(required=True)
     comment=db.TextProperty(required=True)
+    user_name=db.TextProperty(required=True)
